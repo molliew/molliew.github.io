85a9244cabbd1ba2a3be99702d4d1ff8325a12d1
Mollie <molliewolfe@Ryans-MacBook.local>
2015-12-15 18:24:44 -0500

    Final Project JS file
    Committing by pseudo code for my final project

---
+++ final-project/js/project.js
@@ -0,0 +1,6 @@
+// user types character string into input box
+// user clicks button to create image
+// fire click event
+// run function to check what characters were inputted
+// convert inputted characters to alphabite characters
+// display character string as alphabite string
